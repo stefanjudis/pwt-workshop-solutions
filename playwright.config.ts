c5e2c31b6ad6811c0486b07d9a17deb39b68f5a0
stefan judis <stefanjudis@gmail.com>
2023-10-04 23:39:26 +0300

    adjust timeouts

--- playwright.config.ts
+++ playwright.config.ts
@@ -28,6 +28,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on",
+    actionTimeout: 10000,
   },
 
   /* Configure projects for major browsers */
