272f78adce716202ce01272196ad1e51a9e80611
stefan judis <stefanjudis@gmail.com>
2023-10-04 20:17:10 +0300

    add example

---
+++ tests/03-01-requests.api.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from "@playwright/test";
+
+function validateProduct(product) {
+  for (let variant of product.variants) {
+    expect.soft(variant.price.amount).toBeDefined();
+    expect.soft(variant.price.currencyCode).toBeDefined();
+  }
+}
+
+test.describe("API testing", async () => {
+  test.only("make requests", async ({ request }) => {
+    const response = await request.get("/api/products/");
+    expect(response).toBeOK();
+
+    const products = await response.json();
+
+    for (let product of products) {
+      validateProduct(product);
+    }
+    expect(products.length).toBe(15);
+});
