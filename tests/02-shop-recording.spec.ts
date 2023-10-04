dc27d9858109c9c1488cba39d4a170d04530b8eb
stefan judis <stefanjudis@gmail.com>
2023-10-04 17:22:03 +0300

    add recording solutions

---
+++ tests/02-shop-recording.spec.ts
@@ -0,0 +1,41 @@
+import { test } from "@playwright/test";
+
+test.describe("learn how to record", () => {
+  test("add two home products to cart", async ({ page }) => {
+    await page.goto("/");
+    await page
+      .getByTestId("hero-product-grid")
+      .getByRole("link", {
+        name: "The Collection Snowboard: Liquid The Collection Snowboard: Liquid 749.95EUR",
+      })
+      .click();
+    await page.getByLabel("Add item to cart").click();
+    await page.getByLabel("Close cart").click();
+    await page
+      .getByRole("navigation")
+      .getByRole("link", { name: "PWT Workshop logo PWT Workshop" })
+      .click();
+    await page
+      .getByRole("link", {
+        name: "The Multi-managed Snowboard The Multi-managed Snowboard 629.95EUR",
+      })
+      .click();
+    await page.getByLabel("Add item to cart").click();
+  });
+
+  test("search for 'hydrogen' and add one product to cart", async ({
+    page,
+  }) => {
+    await page.goto("/");
+    await page.getByPlaceholder("Search for products...").fill("hydrogen");
+    await page.getByPlaceholder("Search for products...").press("Enter");
+    await page
+      .getByRole("link", {
+        name: "The Collection Snowboard: Liquid The Collection Snowboard: Liquid 749.95",
+      })
+      .first()
+      .click();
+    await page.getByLabel("Add item to cart").click();
+    await page.getByLabel("Close cart").click();
+  });
+});
