883dc71370009635e3a9dbfde77c431e419aedac
stefan judis <stefanjudis@gmail.com>
2023-10-04 20:08:56 +0300

    add example

---
+++ tests/02-10-end-boss.spec.ts
@@ -0,0 +1,65 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("end boss", async () => {
+  test.setTimeout(90_000);
+
+  test.only("make an order", async ({ page }, testInfo) => {
+    await page.goto("/");
+
+    await page
+      .getByTestId("hero-product-grid")
+      .getByRole("link")
+      .first()
+      .click();
+    await page.getByLabel("Add item to cart").click();
+    await expect(page.getByLabel("Add item to cart")).toBeDisabled();
+    await page.getByRole("link", { name: "Proceed to Checkout" }).click();
+
+    await test.step("fill in shipping info", async () => {
+      await page
+        .getByPlaceholder("Email or mobile phone number")
+        .fill("stefanjudis@gmail.com");
+      await page.getByLabel("Country/Region").selectOption("GR");
+      await page.getByPlaceholder("Last name").fill("Judis");
+      await page.getByPlaceholder("Address").fill("The Athens Concert Hall");
+      await page.getByPlaceholder("Postal code").fill("115 21");
+      await page.getByPlaceholder("City").fill("Athens");
+      await expect(page.getByText("Calculating...")).not.toBeVisible();
+    });
+
+    await page.getByText("Standard International").click();
+
+    await test.step("fill in credit card info", async () => {
+      await page
+        .frameLocator('iframe[title="Field container for: Card number"]')
+        .getByPlaceholder("Card number")
+        .fill("1");
+      await page
+        .frameLocator(
+          'iframe[title="Field container for: Expiration date (MM / YY)"]'
+        )
+        .getByPlaceholder("Expiration date (MM / YY)")
+        .fill("11 / 24");
+      await page
+        .frameLocator('iframe[title="Field container for: Security code"]')
+        .getByPlaceholder("Security code")
+        .fill("111");
+      await page
+        .frameLocator('iframe[title="Field container for: Name on card"]')
+        .getByPlaceholder("Name on card")
+        .fill("Stefan Judis");
+    });
+
+    // repeat the submit order action in case it fails
+    await expect(async () => {
+      await expect(page.getByText("Calculating...")).not.toBeVisible();
+      await page.getByRole("button", { name: "Review order" }).click();
+      await page.waitForURL(/review/, { timeout: 10000 });
+    }).toPass({ timeout: 40000 });
+
+    await page.getByRole("button", { name: "Pay now" }).click();
+    await expect(page.getByRole("heading", { name: "Thank you!" })).toBeVisible(
+      { timeout: 20000 }
+    );
+  });
+});
